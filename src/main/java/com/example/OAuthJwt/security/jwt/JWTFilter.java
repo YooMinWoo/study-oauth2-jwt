cff02e696ae09634cea4db0133bec1a184665a5a
YooMinWoo <alsn99@codeline.kr>
2024-12-02 17:03:34 +0900

    test signup(전화번호, 인증번호)

--- src/main/java/com/example/OAuthJwt/security/jwt/JWTFilter.java
+++ src/main/java/com/example/OAuthJwt/security/jwt/JWTFilter.java
@@ -26,14 +26,17 @@ public class JWTFilter extends OncePerRequestFilter {
         //cookie들을 불러온 뒤 Authorization Key에 담긴 쿠키를 찾음
         String authorization = null;
         Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
+        if(cookies != null){
+            for (Cookie cookie : cookies) {
 
-            System.out.println(cookie.getName());
-            if (cookie.getName().equals("Authorization")) {
+                System.out.println(cookie.getName());
+                if (cookie.getName().equals("Authorization")) {
 
-                authorization = cookie.getValue();
+                    authorization = cookie.getValue();
+                }
             }
         }
+
 
         //Authorization 헤더 검증
         if (authorization == null) {
--- src/main/java/com/example/OAuthJwt/security/oauth/CustomClientRegistrationRepo.java
+++ src/main/java/com/example/OAuthJwt/security/oauth/CustomClientRegistrationRepo.java
@@ -1,6 +1,7 @@
 package com.example.OAuthJwt.security.oauth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
@@ -11,6 +12,7 @@ public class CustomClientRegistrationRepo {
 
     private final SocialClientRegistration socialClientRegistration;
 
+    @Bean
     public ClientRegistrationRepository clientRegistrationRepository(){
         return new InMemoryClientRegistrationRepository(
                 socialClientRegistration.naverClientRegistration(),
